b1c18ee2e6f12d11793b0d5fa2cac2c371e0154d
David Hernández <daernandez@gmail.com>
2018-11-17 23:29:48 +0100

    airlineflights exercise modified
    2- Veo has hecho dos queries, deberías agruparlo en una sola.
    3- Te falta el order by
    4- Te falta el order by
    5- En vez de un SUM deberias utilizar un COUNT (Ahora te funciona porque usa un 1 en los cancelados, pero sino no coincidiria con el número de cancelados), también te falta ponerle el Where…
    6-Ojo! Te pide los 10 aviones con más distancia recorrida, no los 10 vuelos de más distancia (el identificador del avión es el TailNum).

--- exercises/02-usflights/airlineflights.sql
+++ exercises/02-usflights/airlineflights.sql
@@ -1,86 +1,65 @@
-#1. Cantidad de registros de la tabla flights
-use usairlineflights
-select count(*) from flights;
+## 1. Cantidad de registros de la tabla flights
 
-#2.1 Retraso medio de salida de aeropuertos de origen
-select Origin, AVG(DepDelay) from flights
+select count(*) from usairlineflights.Flights;
+
+
+## 2.1 Retraso medio de salida de aeropuertos de origen
+
+select usairlineflights.Flights.Origin,
+	avg(Flights.DepDelay) as delay_average,
+    avg(Flights.ArrDelay) as arrival_average 
+from Flights
 group by Origin;
 
-#2.2 Retraso medio de llegada de aeropuertos de origen
-select Origin, AVG(ArrDelay) from flights
-group by Origin;
 
-#3. Retraso medio de llegada de vuelos por Meses y según Origen.
+## 2.2 Retraso medio de llegada de aeropuertos de origen
+
+select Origin, Year, Month, 
+	avg(ArrDelay) as AverageArrivalDelay 
+from Flights 
+group by Origin, Year, Month;
+
+## 3. Retraso medio de llegada de vuelos por Meses y según Origen.
 #Con orden Year, Month 
-select Origin, Year, Month, AVG(ArrDelay) as 'retraso' from flights
-group by Year, Month;
 
-#4. Retraso medio de llegada de vuelos por Meses y según Origen.
+select Origin, Year, Month, DayOfMonth, avg(ArrDelay) 
+from usairlineflights.Flights 
+group by Origin, Year, Month, DayOfMonth
+order by Origin;
+
+
+## 4. Retraso medio de llegada de vuelos por Meses y según Origen.
 #Pero sustituyendo el código del aeropuerto por la ciudad.
-select city, Year, Month, AVG(ArrDelay) as 'retraso' from flights,airports
-where iata=Origin
-group by Year, Month;
 
-#IGUAL usando INNER JOIN
-select city, Year, Month, AVG(ArrDelay) as 'retraso' from flights
-inner join airports
-on iata=Origin
-group by Year, Month;
-
-#5. Compañías con más vuelos cancelados.
-#Ordenadas la de mayor vuelos cancelados primero
-
-select UniqueCarrier, sum(Cancelled) as 'resultado' from flights
-group by UniqueCarrier
-order by resultado DESC;
-
-#OR
-
-select Description, sum(Cancelled) as 'resultado' from flights
-inner join carriers
-on code=UniqueCarrier
-group by UniqueCarrier
-order by resultado DESC;
+select City, Year, Month, 
+	avg(ArrDelay) as AverageArrivalDelay 
+from usairlineflights.Flights 
+inner join usairlineflights.airports on IATA = Origin
+group by Origin, Year, Month;
 
 
-#6. El ID de los aviones que más distancia han recorrido haciendo vuelos
+## 5. Compañías con más vuelos cancelados. Ordenadas la de mayor vuelos cancelados primero
 
-Select FlightNum, Distance from flights
-group by FlightNum
-order by Distance DESC
-limit 10; 
-
-#Igual pero con compañía
-
-Select Description, FlightNum, Distance from flights
-inner join carriers
-on code=UniqueCarrier
-group by FlightNum
-order by Distance DESC
-limit 10; 
+select Carriers.Description as Company, count(*) as Canceled 
+from usairlineflights.Carriers
+left join usairlineflights.Flights on usairlineflights.carriers.Code = usairlineflights.Flights.UniqueCarrier
+where Flights.Cancelled = 1
+group by Carriers.Code, Flights.Cancelled 
+order by Flights.Cancelled asc;
 
 
-#7. Compañías con su retraso promedio,
-#sólo las que sus vuelos lleguen a su destino con un retraso promedio mayor a 10 minutos
+## 6. El ID de los aviones que más distancia han recorrido haciendo vuelos
 
-select UniqueCarrier, avg(DepDelay) as 'promedio' from flights
-group by UniqueCarrier
-having avg(DepDelay) > 10
-order by promedio ASC;
+select TailNum, sum(Distance) as Total
+from usairlineflights.Flights
+group by TailNum
+order by total desc
+limit 10;
 
-#OR
 
-select Description, avg(DepDelay) as 'promedio' from flights
-inner join carriers
-on Code=UniqueCarrier
-group by UniqueCarrier
-having avg(DepDelay) > 10
-order by promedio ASC;
+## 7. Compañías con su retraso promedio: sólo las que sus vuelos lleguen a su destino con un retraso promedio mayor a 10 minutos
 
-#OR
-
-select Description, avg(DepDelay) as 'promedio' from flights, carriers
-where Code=UniqueCarrier
-group by UniqueCarrier
-having avg(DepDelay) > 10
-order by promedio ASC;
+select Flights.UniqueCarrier as Company, avg(Flights.ArrDelay) as Delay
+from usairlineflights.Flights
+group by Flights.UniqueCarrier
+having avg(Flights.ArrDelay) > 10;
