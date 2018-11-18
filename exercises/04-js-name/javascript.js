d52dd9dce32e3eecc1ec3afa9c483301177499e0
David Hernández <daernandez@gmail.com>
2018-11-18 02:22:20 +0100

    Javascript name exercise added

---
+++ exercises/04-js-name/javascript.js
@@ -0,0 +1,41 @@
+var myName;
+myName = ["D", "A", "V", 1, "D"];
+
+var mySurname;
+mySurname = ["H","E","R","N","A","N","D","E","Z"];
+
+var vowel;
+vowel = ["A", "E", "I", "O", "U"];
+
+var myMap = new Map();
+
+//for
+
+for (var i = 0; i < myName.length; i++) {
+
+    if (vowel.indexOf(myName[i]) != -1) {
+        console.log(myName[i]+" is vowel");
+    } else if (typeof myName[i] === 'number') {
+        console.log(myName[i]+" is number");
+    } else {
+        console.log(myName[i]+" is consonant");
+    }
+
+//map
+
+    if (myMap.has(myName[i]) == true) {
+        var valor = myMap.get(myName[i]);
+        myMap.set(myName[i], valor + 1);
+    } else {
+        myMap.set(myName[i], 1);
+    }
+} 
+
+console.log(myName);
+console.log(myMap);
+
+
+//myFullName
+var myFullName = [];
+myFullName.push (...myName, ..." ", ...mySurname);
+console.log(myFullName);
