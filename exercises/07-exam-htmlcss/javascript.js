954eda5a93c7ed342e22db228b6b946d9a0e396b
David Hernández <daernandez@gmail.com>
2018-11-19 15:07:38 +0100

    Testing exam code

---
+++ exercises/07-exam-htmlcss/javascript.js
@@ -0,0 +1,46 @@
+//variables
+/*
+var a=12;
+var b=3;
+sum=a+b;
+
+
+console.log(sum)
+
+//OTRO
+
+a=10
+b="10"
+
+if(a===b){
+    console.log("es verdad")
+}else{
+    console.log("es falso")
+}
+
+//otro ejercicio
+
+function suma(x, y) {
+    document.writeIn("x = " + x);
+    document.writeIn("y = " + y);
+
+    return x+y;
+}
+
+suma(1);
+
+*/
+
+var x = 1;
+if (x == 1) {
+    function demo1(x){
+        return x*2;
+    }
+}
+else{function demo1(x){
+    return x*10;
+    }
+}
+
+x = demo1(3);
+document.writeIn(x);
