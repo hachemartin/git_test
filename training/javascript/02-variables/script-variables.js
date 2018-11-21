d63b01347f95e7ff716d0252e47480c036e4423e
David Hernández <daernandez@gmail.com>
2018-11-21 12:27:13 +0100

    Variables training files updated
    with commentaries in the js file

---
+++ training/javascript/02-variables/script-variables.js
@@ -0,0 +1,13 @@
+// Variables 
+
+// Declaración de variables
+var country = "Spain";
+var continent = "Europe";
+var planet = "Earth";
+
+// Concadenación de dos cadenas de texto
+var country_and_continent = country+" "+continent;
+
+// Lo mostramos con console.log
+console.log(country, continent, planet);
+console.log(country_and_continent);
