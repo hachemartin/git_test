bd711292773553690793262b8aa6f51e18178744
David Hernández <daernandez@gmail.com>
2018-11-22 00:45:04 +0100

    Updating files

---
+++ training/javascript/05-operadores/script-operadores.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// Operadores 
+var number1 = 5;
+var number2 = 10;
+var operation = number1 * number2;
+
+console.log("el resultado de la operación es: "+operation);
+// expected output: 50
+
