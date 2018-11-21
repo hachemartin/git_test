9121cc70173ca560e8c2579c4f6e7e43691bf90d
David Hernández <daernandez@gmail.com>
2018-11-21 12:25:44 +0100

    Updating javascript training
    javascript training files with commentaries

---
+++ training/javascript/01-hello-world/script-hello-world.js
@@ -0,0 +1,5 @@
+// Alert, write and console log examples
+
+alert("Hello World! script out HTML (alert)");
+document.write("Hello World! out HTML (print)");
+console.log("Hello World! out HTML (console)")
