105d07837a8682b3c3123d7e2c2da8cfaeb891d5
David Hernández <daernandez@gmail.com>
2018-11-21 12:37:41 +0100

    New: Chaplin (The Great Dictator) wants censore HTML from JS
    Charles Chaplin wants censore our original HTML from one script file (JS)
    Is it possible?

---
+++ training/javascript/01-hello-world/censored/censored.js
@@ -0,0 +1,29 @@
+// Queremos censurar el contenido H1 y P del HTML
+// Primero creamos variable y seleccionamos
+// Segundo damos valor
+// Ejemplo: https://developer.mozilla.org/es/docs/Learn/Getting_started_with_the_web/JavaScript_basics
+
+var miTitulo = document.querySelector('h1');
+miTitulo.innerHTML = "h1 Censured!";
+
+// Y creamos otra para P
+
+var miParrafo = document.querySelector('p');
+miParrafo.innerHTML = "p Censured!";
+
+// Y creamos otra para LI
+
+var miLista = document.querySelector('li','li');
+miLista.innerHTML = "li Censured!";
+// Observamos que sólo modifica el primer LI
+
+var miImage = document.querySelector('img');
+
+miImage.onclick = function () {
+    var miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/firefox-icon.png') {
+      miImage.setAttribute('src','images/chaplin.jpg');
+    } else {
+      miImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
