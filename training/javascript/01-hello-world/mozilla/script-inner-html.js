7bb28e1a8829a7ec8ff6a0e526754e3106194625
David Hernández <daernandez@gmail.com>
2018-11-21 12:30:38 +0100

    Example innerHTML from Mozilla docs website

---
+++ training/javascript/01-hello-world/mozilla/script-inner-html.js
@@ -0,0 +1,12 @@
+// Modificamos H1
+// Primero creamos variable y seleccionamos
+// Segundo damos valor
+// Ejemplo: https://developer.mozilla.org/es/docs/Learn/Getting_started_with_the_web/JavaScript_basics
+
+var miTitulo = document.querySelector('h1');
+miTitulo.innerHTML = "Hello world!";
+
+// Y vreamos otra para P
+
+var miParrafo = document.querySelector('p');
+miParrafo.innerHTML = "Este texto ha sido reemplazado";
