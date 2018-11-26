b1a4224eaecb8aa1ae6136a472c06d482cd82cec
David Hernández <daernandez@gmail.com>
2018-11-26 10:54:32 +0100

    files updated

--- training/javascript/07-if/script-if.js
+++ training/javascript/07-if/script-if.js
@@ -3,12 +3,17 @@
 // condicional if
 // declaro variables
 var edad1 = 30;
-var edad2 = 50;
+var edad2 = 18;
 
 // la condición
 if(edad1 > edad2){
 	// ejecuta esto
 	console.log(edad1+" es mayor que "+edad2);
+	if(edad1 == 30){
+		console.log("¡Tienes "+edad1+" años!");
+	}else{
+		console.log("No tienes "+edad1+" años")
+	}
 }else{
 	console.log(edad1+" es menor que "+edad2);
 }
