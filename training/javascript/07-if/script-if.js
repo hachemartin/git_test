c043cbf4512364a4551dbaeca15abdeda4338703
David Hernández <daernandez@gmail.com>
2018-11-26 10:46:38 +0100

    training files updated

---
+++ training/javascript/07-if/script-if.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// condicional if
+// declaro variables
+var edad1 = 30;
+var edad2 = 50;
+
+// la condición
+if(edad1 > edad2){
+	// ejecuta esto
+	console.log(edad1+" es mayor que "+edad2);
+}else{
+	console.log(edad1+" es menor que "+edad2);
+}
