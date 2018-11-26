95d677a4c809ccedb33f7a1eb9dcbd1603c84bfc
David Hernández <daernandez@gmail.com>
2018-11-26 13:08:13 +0100

    for loop files added

---
+++ training/javascript/09-for/script-for.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// for
+// quiero mostrar todos los números del 1 al 100
+var number = 100;
+
+// usamos un bucle for
+// 1. Contador que se llama 'var i', una variable i inicializada a cero
+// 2. Condición del bucle hasta, 'i > 100' mientras sea menor que 100
+// 3. Incrementador del bucle, 'i++' con el operador ++
+for(var i = 0; i < 100; i++){
+	console.log("Vamos por el número "+i);
+
+// debugger para visualizar en consola cada bucle
+	debugger;
+}
