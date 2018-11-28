ce6b48fe682c4e30f72004e4051be30f24930be4
David Hernández <daernandez@gmail.com>
2018-11-28 11:54:01 +0100

    Exercise prompt files updated

--- training/javascript/13-exercise-prompt/script-exercise-prompt.js
+++ training/javascript/13-exercise-prompt/script-exercise-prompt.js
@@ -16,6 +16,12 @@ Esquema:
 var numero1 = parseInt(prompt("Introduce el primer número", 0));
 var numero2 = parseInt(prompt("Introduce el segundo número", 0));
 
+// plus: introducimos un 'while' para que sólo se admitan números positivos
+while(numero1 <= 0 || numero2 <= 0 || isNaN(numero1) || isNaN(numero2)){
+	numero1 = parseInt(prompt("Introduce el primer número", 0));
+	numero2 = parseInt(prompt("Introduce el segundo número", 0));
+}
+
 console.log(numero1, numero2);
 
 if(numero1 > numero2){
