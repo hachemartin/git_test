5a3260a944cc3ce1680085fe398b3131c278cbff
David Hernández <daernandez@gmail.com>
2018-11-29 09:24:55 +0100

    file added, but error in second 'for'

---
+++ training/javascript/21-exercise-/script-exercise-table-multiplication.js
@@ -0,0 +1,28 @@
+'use strict'
+/*
+Hacer un programa que muestre la tabla de multiplicar del número introducido.
+
+Esquema:
+prompt que nos pida número
+bucle for que itere hasta un número máximo e incremente
+document.write mostrando resultado
+
+*/
+// declaramos variables que pidan números, que sean enteros
+var numero = parseInt(prompt("Introduce un número",1));
+
+// creamos un encabezado
+document.write("<h1>Tabla del "+numero+"</h1><br/>");
+// bucle for
+for(var i = 1; i <= 10; i++){
+	document.write(i+" x "+numero+" = "+(i*numero)+"<br/>");
+}
+
+// todas las tablas hasta el 7
+
+for(var c = 1; c <= 7; c++){
+	document.write("<h1>Tabla del "+c+"</h1>");
+	for(var i = 1; i <= 10; i++){
+		document.write(i+" x "+c+" = "+(i*c)+"<br/>");
+	}
+}
