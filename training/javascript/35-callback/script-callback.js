34dd634de879275a254eca717ccb7126dcc38bb5
David Hernández <daernandez@gmail.com>
2018-12-14 12:23:23 +0100

    Script callback modified

--- training/javascript/35-callback/script-callback.js
+++ training/javascript/35-callback/script-callback.js
@@ -2,13 +2,27 @@
 
 // función de callback
 
-function adding(number1, number2, addAndShow, addXtwo){
+function adding(number1, number2, addAndShow, addXtwo, divideXtwo, multipleX2){
     var sum = number1 + number2;
 
     addAndShow(sum);
     addXtwo(sum);
+    divideXtwo(sum);
+    multipleX2(sum);
 
     return sum;
 }
 
-adding
+adding(50, 50, function(value){
+    console.log("La suma es: ", value);
+},
+function(value){
+    console.log("La suma por dos es: ", (value*2));
+},
+function(value){
+    console.log("La división entre 2 es: ", (value/2));
+},
+function(value){
+    console.log("Dividido entre 5 es: ", (value/5));
+}
+);
